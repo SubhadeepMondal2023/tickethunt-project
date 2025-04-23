120657bc7551c74fe74c020c809abb8f8eac353f
Subha <msubhadeep9635@gmail.com>
2025-04-24 03:57:26 +0530

    fixed image load issue

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      { hostname: "upbeat-stoat-959.convex.cloud", protocol: "https" },
-      { hostname: "wary-anaconda-29.convex.cloud", protocol: "https" },
+      { hostname: "resilient-boar-393.convex.cloud", protocol: "https" },
+      { hostname: "resilient-boar-393.convex.cloud", protocol: "https" },
     ],
   },
 };
